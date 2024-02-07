3d87dc0a38695a11daea38ab04b86305a81eba5f
Sourabh Kumar <kumar.sou@northeastern.edu>
2024-02-07 17:09:29 -0500

    update setup files

--- setup.sh
+++ setup.sh
@@ -16,8 +16,9 @@ if [ ! -d "venv" ]; then
     python -m venv venv
 fi
 
-# Activate virtual environment
+echo "Sourcing virtual environment from: $PROJECT_PATH/venv/bin/activate"
 source "$PROJECT_PATH"/venv/bin/activate
+echo "Virtual environment activated"
 
 # Upgrade pip
 python -m pip install --upgrade pip
@@ -26,17 +27,16 @@ python -m pip install --upgrade pip
 python -m pip install -r requirements/requirements.txt -r requirements/dev-requirements.txt
 
 # Check if there are pending migrations
-if python manage.py makemigrations --check myapp 2>&1 | grep -q "No changes detected"; then
+if python manage.py makemigrations myapp --check | grep -q "No changes detected"; then
     echo "No pending migrations found."
 else
     # SQL query to delete migration records for the myapp app
-    SQL_QUERY="DELETE FROM django_migrations WHERE app='$DB_NAME';"
+    SQL_QUERY="DELETE FROM django_migrations WHERE app='$DATABASE_NAME';"
 
     # Execute the SQL query using MySQL command line client
-    mysql -u"$DB_USER" -p"$DB_PASSWORD" -h "$DB_HOST" "$DB_NAME" -e "$SQL_QUERY"
+    mysql -u"$DATABASE_USER" -p"$DATABASE_PASSWORD" -h "$DATABASE_HOST" "$DATABASE_NAME" -e "$SQL_QUERY"
 
     # Check if the command executed successfully
-    # shellcheck disable=SC2181
     if [ $? -eq 0 ]; then
       echo "Migration records for myapp deleted successfully."
       python manage.py flush --no-input
