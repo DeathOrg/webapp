311970afaf7973fc4e4756d5dabc4f69b0c2ec36
Sourabh Kumar <kumar.sou@northeastern.edu>
2024-02-07 15:11:07 -0500

    update ci and add test

--- setup.sh
+++ setup.sh
@@ -23,9 +23,37 @@ python -m pip install --upgrade pip
 # Install requirements
 python -m pip install -r requirements/requirements.txt -r requirements/dev-requirements.txt
 
-# Apply database migrations
-python manage.py makemigrations
-python manage.py migrate
+# Check if there are pending migrations
+if python manage.py makemigrations --check myapp 2>&1 | grep -q "No changes detected"; then
+    echo "No pending migrations found."
+else
+    # Define database credentials
+    DB_NAME="webApp"
+    DB_USER="dev-1"
+    DB_PASSWORD="DreamBig@08"
+    DB_HOST="localhost"
+
+    # SQL query to delete migration records for the myapp app
+    SQL_QUERY="DELETE FROM django_migrations WHERE app='myapp';"
+
+    # Execute the SQL query using MySQL command line client
+    mysql -u"$DB_USER" -p"$DB_PASSWORD" -h "$DB_HOST" "$DB_NAME" -e "$SQL_QUERY"
+
+    # Check if the command executed successfully
+    if [ $? -eq 0 ]; then
+      echo "Migration records for myapp deleted successfully."
+      python manage.py flush --no-input
+      python manage.py makemigrations myapp
+      python manage.py migrate
+    else
+      echo "Failed to delete migration records for myapp."
+    fi
+fi
+
+
+#python manage.py flush --noinput
+#python manage.py makemigrations
+#python manage.py migrate
 
 # Run the development server
 python manage.py runserver
