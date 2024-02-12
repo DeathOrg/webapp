412ba1353dffe94c5cefe4d823ef548f11b7d453
Sourabh Kumar <kumar.sou@northeastern.edu>
2024-02-12 16:12:53 -0500

    add healthz and create user tests

--- myapp/test.py
+++ myapp/test.py
@@ -1,9 +1,53 @@
-from django.test import SimpleTestCase
-from django.urls import reverse
+# tests.py
 
-class PingAPITest(SimpleTestCase):
-    def test_ping_view(self):
-        url = reverse('ping')  # Assuming 'ping' is the name of your ping API endpoint
-        response = self.client.get(url)
+from django.test import TestCase, Client
+from django.urls import reverse
+from myapp.models import User
+import base64
+import json
+
+
+class HealthzViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_healthz_get(self):
+        response = self.client.get(reverse('healthz'))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()['message'], 'pong')
+
+    def test_healthz_post(self):
+        response = self.client.post(reverse('healthz'))
+        self.assertEqual(response.status_code, 405)
+
+
+class PingViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_ping_get(self):
+        response = self.client.get(reverse('ping'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'message': 'pong'})
+
+    def test_ping_post(self):
+        response = self.client.post(reverse('ping'))
+        self.assertEqual(response.status_code, 405)
+
+
+class CreateUserViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user_data = {'username': 'testuser@example.com', 'password': 'testpassword', 'first_name': 'Rahul',
+                          'last_name': 'Kumar',
+                          }
+
+    def test_create_user_success(self):
+        response = self.client.post(reverse('create_user'), data=json.dumps(self.user_data),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(User.objects.filter(username='testuser@example.com').exists())
+
+    def test_create_user_missing_data(self):
+        response = self.client.post(reverse('create_user'), data=json.dumps({}), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertFalse(User.objects.filter(username='').exists())
