a5d06b0e6b4a4780f51e1bd36f977b8d844b9cb2
Sourabh Kumar <kumar.sou@northeastern.edu>
2024-02-12 18:30:15 -0500

    fix test pipeline minor issue

--- myapp/test.py
+++ myapp/test.py
@@ -2,6 +2,8 @@ from django.test import SimpleTestCase
 from django.urls import reverse
 
 class PingAPITest(SimpleTestCase):
+    databases = ['default']
+
     def test_ping_view(self):
         url = reverse('ping')  # Assuming 'ping' is the name of your ping API endpoint
         response = self.client.get(url)
