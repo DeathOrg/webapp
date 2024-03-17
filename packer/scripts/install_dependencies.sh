0ebd639ce6af3b7898577a1fdf1fb02987b0473d
Sourabh Kumar <kumar.sou@northeastern.edu>
2024-03-17 00:30:10 -0400

    remove mysql client as there is no use case

--- packer/scripts/install_dependencies.sh
+++ packer/scripts/install_dependencies.sh
@@ -13,9 +13,6 @@ echo "Updating System..."
 sudo yum update -y || handle_error "Failed to update system packages."
 sudo yum upgrade -y
 
-echo "Install mysql client..."
-sudo yum install -y mysql || handle_error "Failed to install mysql client"
-
 # Install Python 3.9
 echo "Installing Python 3.9..."
 sudo yum install -y python39 || handle_error "Failed to install Python 3.9."
@@ -24,19 +21,18 @@ sudo yum install -y python39 || handle_error "Failed to install Python 3.9."
 echo "Installing pip for Python 3.9..."
 sudo yum install -y python39-pip || handle_error "Failed to install pip for Python 3.9."
 
+# Upgrade pip
+echo "Upgrading pip..."
+python3.9 -m pip install --upgrade pip
+
 # Install Python and MySQL development packages
 echo "Installing Python and MySQL development packages..."
 sudo yum install -y python39-devel mysql-devel || handle_error "Failed to install Python and MySQL development packages."
 sudo yum groupinstall -y "Development Tools" || handle_error "Failed to install development tools."
 python3.9 -m pip install mysqlclient || handle_error "Failed to install mysqlclient."
 
-# Upgrade pip
-echo "Upgrading pip..."
-python3.9 -m pip install --upgrade pip
-
 # Install requirements
 echo "Installing requirements..."
-#python3.9 -m pip install -r /tmp/requirements.txt
 sudo /usr/bin/pip3.9 install -r /tmp/requirements.txt
 
 echo "pip3.9 list..."
